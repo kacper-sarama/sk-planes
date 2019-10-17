b325c3fd97280aa94301e70ff523d740181473b5
ksarama <ksarama@andea.com>
2019-10-17 09:58:34 +0200

    Mat-error i Mat-hint implemented

--- src/app/flights/new-flight/new-flight.component.ts
+++ src/app/flights/new-flight/new-flight.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
 import { MatDialogRef } from "@angular/material";
+import { FlightFormComponent } from "../flight-form/flight-form.component";
 
 @Component({
   selector: "app-new-flight",
@@ -7,5 +8,7 @@ import { MatDialogRef } from "@angular/material";
   styleUrls: ["./new-flight.component.scss"]
 })
 export class NewFlightComponent {
+  @ViewChild("flightForm", { static: true, read: ElementRef }) flightForm: FlightFormComponent;
+
   constructor(private dialogRef: MatDialogRef<NewFlightComponent>) {}
 }
