56141f6599c454456ce7b04569df370d767a876b
ksarama <ksarama@andea.com>
2019-10-30 15:01:10 +0100

    flight edit - finished

--- src/app/flights/edit-flight/edit-flight.component.ts
+++ src/app/flights/edit-flight/edit-flight.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from "@angular/core";
+import { FlightsService } from "src/app/core/services/flights.service";
+import { ActivatedRoute } from "@angular/router";
+import { FlightFormComponent } from "../flight-form/flight-form.component";
+import { tap } from "rxjs/operators";
+import { Flight } from "src/app/models/flight.model";
 
 @Component({
-  selector: 'app-edit-flight',
-  templateUrl: './edit-flight.component.html',
-  styleUrls: ['./edit-flight.component.scss']
+  selector: "app-edit-flight",
+  templateUrl: "./edit-flight.component.html",
+  styleUrls: ["./edit-flight.component.scss"]
 })
-export class EditFlightComponent implements OnInit {
+export class EditFlightComponent implements OnInit, AfterViewInit {
+  @ViewChild("flightForm", { static: true }) flightForm: FlightFormComponent;
+  flight: Flight;
 
-  constructor() { }
+  constructor(private flightService: FlightsService, private route: ActivatedRoute) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  ngAfterViewInit() {
+    this.loadFlight();
   }
 
+  private loadFlight() {
+    const key = this.route.snapshot.params["key"];
+    this.flightService
+      .getFlight(key)
+      .pipe(tap(flight => this.flightForm.setFlight(flight)))
+      .subscribe(flight => (this.flight = flight));
+  }
 }
