abc660f214f22454e23ccf59dd2911fcd6ecc822
ksarama <ksarama@andea.com>
2019-10-30 15:34:07 +0100

    remove flight - finished

--- src/app/flights/edit-flight/edit-flight.component.ts
+++ src/app/flights/edit-flight/edit-flight.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit, ViewChild, AfterViewInit } from "@angular/core";
 import { FlightsService } from "src/app/core/services/flights.service";
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
 import { FlightFormComponent } from "../flight-form/flight-form.component";
 import { tap } from "rxjs/operators";
 import { Flight } from "src/app/models/flight.model";
+import { MatSnackBar } from "@angular/material";
 
 @Component({
   selector: "app-edit-flight",
@@ -14,12 +15,47 @@ export class EditFlightComponent implements OnInit, AfterViewInit {
   @ViewChild("flightForm", { static: true }) flightForm: FlightFormComponent;
   flight: Flight;
 
-  constructor(private flightService: FlightsService, private route: ActivatedRoute) {}
+  constructor(
+    private flightService: FlightsService,
+    private route: ActivatedRoute,
+    private toast: MatSnackBar,
+    private router: Router
+  ) {}
 
   ngOnInit() {}
 
   ngAfterViewInit() {
     this.loadFlight();
+  }
+
+  removeFlight() {
+    this.flightService
+      .removeFlight(this.flight.key)
+      .then(this.onRemoveSuccess.bind(this), this.onRemoveFailure.bind(this));
+  }
+
+  private onRemoveSuccess() {
+    this.router.navigate(["/dashboard"]);
+    this.toast.open("Flight has been successully removed", "", { panelClass: "toast-success" });
+  }
+
+  private onRemoveFailure(error) {
+    this.toast.open(error.message, "", { panelClass: "toast-error" });
+  }
+
+  editFlight() {
+    this.flightService
+      .editFlight(this.flight.key, this.flightForm.form.value)
+      .then(this.onEditSuccess.bind(this), this.onEditFailure.bind(this));
+  }
+
+  private onEditSuccess() {
+    this.router.navigate(["/dashboard/flights"]);
+    this.toast.open("Flight has been successully edited", "", { panelClass: "toast-success" });
+  }
+
+  private onEditFailure(error) {
+    this.toast.open(error.message, "", { panelClass: "toast-error" });
   }
 
   private loadFlight() {
